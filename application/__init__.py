81e5cb29b4517dc0ebe5a1819abb15820852153d
William Kerley <wjmkerley@gmail.com>
2021-11-17 15:21:16 +0000

    file written to import and create everything needed for application

---
+++ application/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+import application.routes
+import application.models
