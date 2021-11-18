bf8e74dc5ac2cbb2af6c0aa1fe57aebb037ffe30
William Kerley <wjmkerley@gmail.com>
2021-11-18 16:22:30 +0000

    first attempt at route

--- application/routes.py
+++ application/routes.py
@@ -8,17 +8,11 @@ def home():
     if len(items) == 0:
         return "Welcome to a basic todo app. Why not add some todos?"
     else:
-<<<<<<< HEAD
         # don't understand how this works yet, but can move on
         out = ''
         for item in items:
-            print(item)
             out += ''.join(str(item.id) + '||' + item.name + '||' + item.desc + '||' + item.status + '||' + str(item.date)) + '<br>'
         return  out
-=======
-        items = Items.query.all()
-        return '<br>'.join([str(item.id) + '||' + item.name + '||' + item.desc  for item in items]) if items else ''
->>>>>>> 45697d61de570d409df0cab79eaf7d40dad7a088
 
 @app.route('/add/<name>/<desc>')
 def add_item(name, desc):
@@ -26,3 +20,11 @@ def add_item(name, desc):
     db.session.add(newitem)
     db.session.commit()
     return "Added item"
+
+@app.route('/status/<int:id>/<status>')
+def update_status(status):
+    item = Items.query.get(id)
+    item.status = status
+    db.session.add(item)
+    db.session.commit()
+    return "Item status updated"
