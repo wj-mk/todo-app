c44a18ec7db2926786cc1c06eb172b18a0afa514
William Kerley <wjmkerley@gmail.com>
2021-11-18 18:22:28 +0000

    simplified secret key input

--- application/__init__.py
+++ application/__init__.py
@@ -5,7 +5,7 @@ from os import getenv
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = getenv('secret_key')
+app.config['SECRET_KEY'] = getenv('key')
 
 db = SQLAlchemy(app)
 
